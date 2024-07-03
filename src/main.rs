f2bb561b5936a7c1da3c3a014968213045aaefbe
Saketh Kalaga <51327242+renormalize@users.noreply.github.com>
2024-07-03 21:12:12 +0530

    Change `from_ne_bytes` to `from_le_bytes` since `.wav` uses little endian
    * `from_ne_bytes` to `from_le_bytes` since `.wav` uses little endian.
    
    * Add comments which explains why the order for the hardcoded hex is the way it is.
    
    * Remove unnecessary comments in `main.rs`.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
-use std::{error, fs::File};
+use std::error;
 
 pub mod wav;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let wav = wav::Wav::new(String::from("./samples/bad_guitar.wav"))?;
     println!("{:#?}", wav);
-
-    // // Byte 36 onwards is data chunk
-    // let mut counter = 44;
-
-    // while counter < 48 {
-    //     print!("{:08b} ", wav_bytes[counter]);
-    //     counter += 1;
-    // }
-
-    // // TODO: @siddhantbarua @renormalize: Figure out how to interpret data chunks with the padding byte.
-    // // ref: https://wavefilegem.com/how_wave_files_work.html
-    // let first_sample = i32::from_ne_bytes(wav_bytes[44..48].try_into().unwrap());
-    // println!("First sample is {first_sample}");
-
-    // let
-
     Ok(())
 }
--- src/wav.rs
+++ src/wav.rs
@@ -39,6 +39,7 @@ impl Default for FormatChunk {
     }
 }
 
+// the fact chunk indicates the number of frames in the file
 #[derive(Default, Debug)]
 struct FactChunk {
     chunk_id: u32,
@@ -108,17 +109,17 @@ impl Wav {
 // validate_riff_chunk validates the riff chunk and returns the ID, Body Size and Form Type of the chunk
 // TODO: since the sequence of bytes for the IDs is hardcoded and are read in a little endian way, this is not platform independent. This will fail on big endian systems.
 fn validate_riff_chunk(bytes: &[u8]) -> Result<(u32, u32, u32), Box<dyn std::error::Error>> {
-    // Chunk ID must be "RIFF" which is 0x46464952
-    let chunk_id = match u32::from_ne_bytes(bytes[0..4].try_into()?) {
+    // Chunk ID must be "RIFF" which is 0x46464952 when considered a little endian integer
+    let chunk_id = match u32::from_le_bytes(bytes[0..4].try_into()?) {
         0x46464952u32 => 0x46464952u32,
         _ => return Err(WavError::FileNotEncodedProperly.into()),
     };
 
     // Chunk Body Size will be used to read the file contents
-    let chunk_body_size = u32::from_ne_bytes(bytes[4..8].try_into()?);
+    let chunk_body_size = u32::from_le_bytes(bytes[4..8].try_into()?);
 
-    // Riff Form Type  must be "WAVE" which is 0x45564157
-    let form_type = match u32::from_ne_bytes(bytes[8..12].try_into()?) {
+    // Riff Form Type  must be "WAVE" which is 0x45564157 when considered a little endian integer
+    let form_type = match u32::from_le_bytes(bytes[8..12].try_into()?) {
         0x45564157u32 => 0x45564157u32,
         _ => return Err(WavError::FileNotEncodedProperly.into()),
     };
@@ -129,25 +130,25 @@ fn validate_riff_chunk(bytes: &[u8]) -> Result<(u32, u32, u32), Box<dyn std::err
 // validate_format_chunk validates the format chunk and returns the format chunk
 // TODO: since the sequence of bytes for the IDs is hardcoded and are read in a little endian way, this is not platform independent. This will fail on big endian systems.
 fn validate_format_chunk(bytes: &[u8]) -> Result<FormatChunk, Box<dyn std::error::Error>> {
-    // Chunk ID must be 0x20746d66
-    let chunk_id = match u32::from_ne_bytes(bytes[0..4].try_into()?) {
+    // Chunk ID must be 0x20746d66 when considered a little endian integer
+    let chunk_id = match u32::from_le_bytes(bytes[0..4].try_into()?) {
         0x20746d66u32 => 0x20746d66u32,
         _ => return Err(error::WavError::FileNotEncodedProperly.into()),
     };
 
     let format_chunk_fields = FormatChunkFields {
         chunk_id,
-        chunk_body_size: u32::from_ne_bytes(bytes[4..8].try_into()?),
+        chunk_body_size: u32::from_le_bytes(bytes[4..8].try_into()?),
         // place holder format tag, which will not be used in the return
         format_tag: 0u16,
-        num_chan: u16::from_ne_bytes(bytes[10..12].try_into()?),
-        sample_per_sec: u32::from_ne_bytes(bytes[12..16].try_into()?),
-        avg_bytes_per_sec: u32::from_ne_bytes(bytes[16..20].try_into()?),
-        block_align: u16::from_ne_bytes(bytes[20..22].try_into()?),
-        bits_per_sample: u16::from_ne_bytes(bytes[22..24].try_into()?),
+        num_chan: u16::from_le_bytes(bytes[10..12].try_into()?),
+        sample_per_sec: u32::from_le_bytes(bytes[12..16].try_into()?),
+        avg_bytes_per_sec: u32::from_le_bytes(bytes[16..20].try_into()?),
+        block_align: u16::from_le_bytes(bytes[20..22].try_into()?),
+        bits_per_sample: u16::from_le_bytes(bytes[22..24].try_into()?),
     };
 
-    match u16::from_ne_bytes(bytes[8..10].try_into()?) {
+    match u16::from_le_bytes(bytes[8..10].try_into()?) {
         1u16 => Ok(FormatChunk::IntegerPCM {
             format_chunk_fields: FormatChunkFields {
                 format_tag: 1u16,
@@ -170,23 +171,23 @@ fn validate_format_chunk(bytes: &[u8]) -> Result<FormatChunk, Box<dyn std::error
 // validate_fact_chunk validates the fact chunk and returns the fact chunk
 // TODO: since the sequence of bytes for the IDs is hardcoded and are read in a little endian way, this is not platform independent. This will fail on big endian systems.
 fn validate_fact_chunk(bytes: &[u8]) -> Result<Option<FactChunk>, Box<dyn std::error::Error>> {
-    let chunk_id = u32::from_ne_bytes(bytes[0..4].try_into()?);
+    let chunk_id = u32::from_le_bytes(bytes[0..4].try_into()?);
     match chunk_id {
         0x74636166u32 => Ok(Some(FactChunk {
             chunk_id,
-            chunk_body_size: u32::from_ne_bytes(bytes[4..8].try_into()?),
-            no_sample_frames: u32::from_ne_bytes(bytes[8..12].try_into()?),
+            chunk_body_size: u32::from_le_bytes(bytes[4..8].try_into()?),
+            no_sample_frames: u32::from_le_bytes(bytes[8..12].try_into()?),
         })),
         _ => Ok(None),
     }
 }
 
 fn validate_data_chunk(bytes: &[u8]) -> Result<DataChunk, Box<dyn std::error::Error>> {
-    let chunk_id = u32::from_ne_bytes(bytes[0..4].try_into()?);
-    let chunk_body_size = u32::from_ne_bytes(bytes[4..8].try_into()?);
+    let chunk_id = u32::from_le_bytes(bytes[0..4].try_into()?);
+    let chunk_body_size = u32::from_le_bytes(bytes[4..8].try_into()?);
     Ok(DataChunk {
         chunk_id,
         chunk_body_size,
-        sample_data: vec![],
+        sample_data: bytes[8..].to_vec(),
     })
 }
